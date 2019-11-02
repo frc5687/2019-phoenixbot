fbe9e45b221e7bf6de2c6fba575a25ab8862ca19
Dennis_Slobodzain <dennis.slobodzian@baxter-academy.org>
2019-11-02 13:40:55 -0400

    Base for pheonix bot. Only two controllers as of now.

---
+++ src/main/java/org/frc5687/deepspace/phoenixbot/commands/Drive.java
@@ -0,0 +1,20 @@
+package org.frc5687.deepspace.phoenixbot.commands;
+
+public class Drive extends OutliersCommand {
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    protected void execute() {
+        super.execute();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+}
---
+++ src/main/java/org/frc5687/deepspace/phoenixbot/utils/Version.java
@@ -0,0 +1,12 @@
+package org.frc5687.deepspace.phoenixbot.utils;
+
+/**
+ * A version class to read current Git information from deployed robot code
+ * @author wilstenholme
+ */
+
+public class Version {
+    public static final String BRANCH = "master";
+    public static final String REVISION = "4c4e3de";
+    public static final String MODIFIED = "";
+}
